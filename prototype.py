2b900d90ab5ae78720cd93f5f9803aee9fa38659
lao <laorenfei@163.com>
2017-11-07 00:16:06 +0800

    add new home page

--- prototype.py
+++ prototype.py
@@ -15,30 +15,17 @@ settings.configure(
     INSTALLED_APPS=(
         'django.contrib.staticfiles',
         'sitebuilder',
-
     ),
     TEMPLATES=(
         {
             'BACKEND':'django.template.backends.django.DjangoTemplates',
             'DIRS':[],
             'APP_DIRS':True,
-            'OPTIONS': {
-                'context_processors': [
-                    'django.template.context_processors.debug',
-                    'django.template.context_processors.request',
-                    'django.contrib.auth.context_processors.auth',
-                    'django.contrib.messages.context_processors.messages',
-                ],
-            },
-
         },
     ),
     STATIC_URL='/static/',
     SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR,'pages'),
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, "/sitebuilder/static"), ]
-     #country是生成的APP#
-
-    )
+    ),
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
